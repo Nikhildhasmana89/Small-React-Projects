1121be5efdc727d4f29283d8bfa95c6c3d01aa36
Nikhildhasmana89 <nikhildhasmana5@gmail.com>
2025-08-22 19:52:45 +0530

    implement basic Rock-Paper-Scissors game logic and UI

--- Rock-Paper-Scissors/src/App.jsx
+++ Rock-Paper-Scissors/src/App.jsx
@@ -1,18 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  
+  const choice = ["Rock", "Paper", "scissor"];
+  const [playerstate, setplayerstate] = useState(null);
+  const [computerstate, setcomputerstate] = useState(null);
+  const [resultstate, setresultstate] = useState("");
 
+  const Playerchoice = (choice) => {
+     setplayerstate(choice);
+  
+     
+     const computerchoice = choice[Math.floor(Math.random * choice.length)];
+     setcomputerstate(random);
+     
+     if (choice === computerchoice) {
+       console.log("Its Draw");
+      } else if (
+        (choice === "Rock" && computerchoice === "Scissor") ||
+        (choice === "Paper" && computerchoice === "Rock") ||
+        (choice === "Scissor" && computerchoice === "Paper")
+  ) {
+    //console.log('You Win');
+    resultstate('You Win')
+  } else {
+    //console.log('You Lose');
+    resultstate('You Lose')
+    
+  }
+};
+  
   return (
-   <>
-   <div className='w-full h-screen bg-black'>
-      
-   </div>
-   </>
-  )
+    <>
+      <div className="w-full h-screen bg-black">
+        <div>
+          <h1>Rock-Paper-Scissor Game </h1>
+          <button
+            onClick={() => playerstate('Rock')}
+            className="px-4 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700 transition"
+          >
+            Rock
+          </button>
+          <button className="px-4 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700 transition">
+            Paper
+          </button>
+          <button className="px-4 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700 transition">
+            Scissor
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
